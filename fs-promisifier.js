8e9f75299f7582132b8a3f82fc3b7f96f4e6e7c4
Sean <ussa.say@gmail.com>
2021-08-29 23:09:15 -0700

    Switch to using Jekyll (WIP)

--- fs-promisifier.js
+++
@@ -1,8 +0,0 @@
-module.exports = (fn, ...args) => {
-  return new Promise((res, rej) => {
-    fn(...args, (err, ...output) => {
-      if (err) rej(err);
-      else res(...output);
-    });
-  });
-}
